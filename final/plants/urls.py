fab50b33c59aaa86a904331db70cbdb72971c865
elibrowne <eli.browne.23@bishops.com>
2021-03-22 09:27:27 -0700

    Added post page, improved plant model, added users

--- final/plants/urls.py
+++ final/plants/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-	path('plant/<str:plantname>', views.plant, name='plantinfo'),
-	path('user/<str:username>', views.user, name='userinfo')
+	path('plant/<str:plantname>/', views.plant, name='plantinfo'),
+	path('user/<str:username>/', views.user, name='userinfo'),
+	path('post/<str:post_id>/', views.post, name='postcontent') # why not int?
 ]
--- final/plants/views.py
+++ final/plants/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Plant
+from .models import Post
 
 # Index view
 def index(request):
-	return HttpResponse("Index was accessed!")
+	plant_objects = Plant.objects.order_by('plant_name') # to be passed to the homepage
+	template = loader.get_template('index.html')
+	context = {
+		'plant_objects': plant_objects,
+	}
+	return HttpResponse(template.render(context, request))
 
-# Plant view
+# Plant view (basic)
 def plant(request, plantname):
 	return HttpResponse("You're looking at " + plantname + "'s plant page.")
 
@@ -13,3 +22,12 @@ def plant(request, plantname):
 def user(request, username):
 	return HttpResponse("You're looking at " + username + "'s user page.")
 
+# Post view
+def post(request, post_id):
+	post_objects = Post.objects.order_by('id')
+	template = loader.get_template('post.html')
+	context = {
+		'post': post_objects[int(post_id)],
+		# POST REPLIES OBJECT <3
+	}
+	return HttpResponse(template.render(context, request))
